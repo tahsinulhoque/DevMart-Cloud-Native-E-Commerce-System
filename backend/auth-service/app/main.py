5310a0927537edcd86411a6c3f6beaf78b950661
tahsinulhoque <tahsinulhoquesiddiki@gmail.com>
2026-06-14 22:31:17 +0600

    backend folder created

---
+++ backend/auth-service/app/main.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI
+
+app = FastAPI(
+    title="DevMart Auth Service",
+    version="1.0.0"
+)
+
+@app.get("/")
+def root():
+    return {
+        "message": "DevMart Auth Service Running"
+    }
+
+@app.get("/health")
+def health():
+    return {
+        "status": "healthy"
+    }
