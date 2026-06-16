18cf7d102db0289b9979f57839fba2efa4d38b32
tahsinulhoque <tahsinulhoquesiddiki@gmail.com>
2026-06-16 23:49:12 +0600

    Models update

--- backend/auth-service/app/models/user.py
+++ backend/auth-service/app/models/user.py
@@ -1,5 +1,4 @@
 from sqlalchemy import Column, Integer, String
-
 from app.database.base import Base
 
 
@@ -7,6 +6,6 @@ class User(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, index=True)
-    name = Column(String(100))
-    email = Column(String(100), unique=True, index=True)
+    full_name = Column(String(255))
+    email = Column(String(255), unique=True)
     password = Column(String(255))
