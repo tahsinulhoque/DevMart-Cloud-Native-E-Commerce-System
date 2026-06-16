04f252597e3f04cbdeb91acd23763c9b69429587
tahsinulhoque <tahsinulhoquesiddiki@gmail.com>
2026-06-17 00:30:51 +0600

    security page is update

--- backend/auth-service/app/utils/security.py
+++ backend/auth-service/app/utils/security.py
@@ -6,13 +6,9 @@ pwd_context = CryptContext(
 )
 
 
-def hash_password(password: str):
-    return pwd_context.hash(password)
-
-
 def verify_password(
-    plain_password: str,
-    hashed_password: str
+    plain_password,
+    hashed_password
 ):
     return pwd_context.verify(
         plain_password,
