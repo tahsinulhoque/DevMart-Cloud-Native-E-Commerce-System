07710c7f1b235454886fad760fd5156f16b9120b
tahsinulhoque <tahsinulhoquesiddiki@gmail.com>
2026-06-18 23:11:38 +0600

    update core config file

--- backend/auth-service/app/core/config.py
+++ backend/auth-service/app/core/config.py
@@ -2,9 +2,12 @@ from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
-    SECRET_KEY: str
-    ALGORITHM: str
-    ACCESS_TOKEN_EXPIRE_MINUTES: int
+
+    SECRET_KEY: str = "devmart-super-secret-key"
+
+    ALGORITHM: str = "HS256"
+
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 60
 
     class Config:
         env_file = ".env"
