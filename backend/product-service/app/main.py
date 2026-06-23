4c1000fefba519625cdb2586db7767f3cddc4530
tahsinulhoque <tahsinulhoquesiddiki@gmail.com>
2026-06-24 00:04:46 +0600

    Modified main file

--- backend/product-service/app/main.py
+++ backend/product-service/app/main.py
@@ -1,4 +1,7 @@
 from fastapi import FastAPI
+from sqlalchemy import text
+
+from app.database.db import engine
 
 app = FastAPI(
     title="Product Service",
@@ -9,4 +12,15 @@ app = FastAPI(
 def root():
     return {
         "message": "Product Service Running"
+    }
+
+
+@app.get("/db-test")
+def db_test():
+
+    with engine.connect() as connection:
+        result = connection.execute(text("SELECT 1"))
+
+    return {
+        "database": "connected"
     }
